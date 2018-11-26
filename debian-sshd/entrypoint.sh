47646648359f9da78af3e1b96408d17437b1b903
HR05133 <sundev@126.com>
2018-11-26 13:28:10 +0800

    bug fix

--- debian-sshd/entrypoint.sh
+++ debian-sshd/entrypoint.sh
@@ -2,18 +2,19 @@
 
 id $USERNAME>/dev/null 2>&1
 
-if [ $? -ne 0 ];
+if [ $? -ne 0 ]; then
     # user is not defined
     # 创建默认用户及home目录，并赋予sudo权限
     useradd \
         --create-home \
         --no-user-group \
         --password $(openssl passwd -1 $PASSWORD) \
-        --groups users sudo \
+        --groups users,sudo \
         $USERNAME
-    curl -o /home/$USERNAME/.vimrc \
-        https://raw.githubusercontent.com/sundev126/dotfiles/master/vim/vimrc
+    curl -s -o /home/$USERNAME/.vimrc \
+        https://raw.githubusercontent.com/sundev126/dotfiles/master/vim/vimrc \
+        2>/dev/null
     chown $USERNAME /home/$USERNAME/.vimrc
 fi
 
-exec "/usr/sbin/sshd -D"
+exec "$@"
